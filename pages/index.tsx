950969e528bb3dfd8a1d0cf00b3837d80e43c0bb
Tom Ballinger <tom@convex.dev>
2023-02-22 22:00:14 -0800

    TypeScript import resolution repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import _ from "underscore";
+import { fileInThisRepo } from "underscore";
+console.log(fileInThisRepo);
+
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
---
+++ underscore.ts
@@ -0,0 +1,1 @@
+export const fileInThisRepo = true;
